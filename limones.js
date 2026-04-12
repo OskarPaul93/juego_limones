5670e1a54a722b22b14804df2783b8c10f3a3536
OskarPaul93 <paul_1993oscar@hotmail.com>
2026-04-12 14:56:03 -0500

    agregar bajar limón

--- limones.js
+++ limones.js
@@ -4,11 +4,17 @@ let ctx=canvas.getContext ("2d");
 const ALTURA_SUELO=20;
 const ALTURA_PERSONAJE=60;
 const ANCHO_PERSONAJE=40;
+const ALTURA_LIMON=20;
+const ANCHO_LIMON=20;
+
 let personajeX=canvas.width/2;
+let limonX=canvas.width/2;
+let limonY=5;
 
 function iniciar(){
     dibujarSuelo();
     dibujarPersonaje();
+    dibujarLimon();
 }
 
 function dibujarSuelo(){
@@ -21,6 +27,7 @@ function dibujarPersonaje(){
     ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
 }
 
+//Mover Izquierda
 function moverIzquierda(){
     personajeX=personajeX-10;
     actualizarPantalla(); 
@@ -36,8 +43,20 @@ function actualizarPantalla(){
     limpiarCanva();
     dibujarSuelo();
     dibujarPersonaje();
+    dibujarLimon();
 }
 
 function limpiarCanva(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
+}
+
+function dibujarLimon(){
+    ctx.fillStyle= "green";
+    ctx.fillRect(limonX,limonY,ANCHO_LIMON,ALTURA_LIMON);
+}
+
+function bajarLimon(){
+    limonY=limonY+10;
+    dibujarLimon();
+    actualizarPantalla();
 }
